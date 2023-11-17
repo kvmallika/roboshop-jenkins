afa01a1ffedb0c28224aac2ae63f3da74e811a89
Mallika <kmallika.malli@gmail.com>
2023-11-17 11:42:06 +0530

    added groovy files

---
+++ vars/nodejs.groovy
@@ -0,0 +1,44 @@
+def call(){
+    pipeline {
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        options {
+            ansiColor('xterm')
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+
+        stages {
+            stage('Code Quality') {
+                steps {
+                    sh 'sonar-scanner -Dsonar.projectkey=${component}'
+                }
+            }
+            stage('Unit test cases') {
+                steps {
+                    sh 'echo Unit Test'
+                }
+            }
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo CheckMarx Scan'
+                }
+            }
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'CheckMarx SCA Scan'
+                }
+            }
+        }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}
