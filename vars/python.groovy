e5bb02d7deb33a28c5955bff0efa44cf847f153e
Mallika <kmallika.malli@gmail.com>
2023-11-24 10:47:36 +0530

    Changes in the code compile

--- vars/python.groovy
+++ vars/python.groovy
@@ -12,7 +12,7 @@ def call(){
         stages {
             stage('Code Quality') {
                 steps {
-                    sh 'echo Code Quality'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.47.250:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
             stage('Unit test cases') {
