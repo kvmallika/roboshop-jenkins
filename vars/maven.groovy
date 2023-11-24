1b0a407f1eb14328e1ad1feb9eba74d51860a019
Mallika <kmallika.malli@gmail.com>
2023-11-24 10:07:43 +0530

    added Jenkins file in shipping

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -20,7 +20,7 @@ def call(){
             }
             stage('Code Quality') {
                 steps {
-                    sh 'echo Code Quality'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.47.250:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
             stage('Unit test cases') {
