19c373fc32f3346adb01db91b57d133f11093840
Mallika <kmallika.malli@gmail.com>
2023-12-04 11:57:29 +0530

    added when expression in nodejs file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -34,7 +34,7 @@ def call(){
             stage('Release Application') {
                 when {
                     expression {
-                        TAG_NAME ==~ ".*"
+                        env.TAG_NAME ==~ ".*"
                     }
                 }
                 steps {
