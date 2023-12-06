50f1c8a52854acfb734376a24e9a163c4f745848
Mallika <kmallika.malli@gmail.com>
2023-12-06 11:24:39 +0530

    added secrets NEXUS_USR and NEXUS_PSW

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,6 +8,9 @@ def call(){
         options {
             ansiColor('xterm')
         }
+        environment {
+            NEXUS = credentials('NEXUS')
+        }
         stages {
             stage('Code Quality') {
                 steps {
@@ -41,7 +44,7 @@ def call(){
                     sh 'npm install'
                     sh 'echo ${TAG_NAME} >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
-                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.32.14:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.32.14:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
         }
