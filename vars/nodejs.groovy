e63e6329cb3a16b1a3efe7c89b6ea796110d72d2
Mallika <kmallika.malli@gmail.com>
2023-12-08 11:45:49 +0530

    added schema_dir file in nodejs.groovy file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -43,7 +43,7 @@ def call(){
                 steps {
                     sh 'npm install'
                     sh 'echo ${TAG_NAME} >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
                     sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.32.14:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
